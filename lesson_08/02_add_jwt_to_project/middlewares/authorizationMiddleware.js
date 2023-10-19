6939c1cda422b50aefb2905205e101b1d4fef83d
Daniel Marchenko <marchenkodanil97@gmail.com>
2023-10-19 22:02:22 +0300

    refactoring auth middleware

--- lesson_08/02_add_jwt_to_project/middlewares/authorizationMiddleware.js
+++ lesson_08/02_add_jwt_to_project/middlewares/authorizationMiddleware.js
@@ -1,33 +1,48 @@
 const requestError = require("../helpers/requestError");
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
+const wrapper = require('../helpers/controllerWrapper')
 
 const { JWT_SECRET } = process.env;
 
 const authorization = async (req, res, next) => {
     const authHeader = req.headers.authorization || "";
-    const [type, token] = authHeader.split(" ");
+    const token = authHeader.split(" ")[1];
 
-    if (type !== "Bearer") {
-        throw requestError(401, "Token type is not allowed");
-    }
-
-    if (!token) {
-        throw requestError(401, "Token is required");
-    }
-
-    try {
-        const { id } = jwt.verify(token, JWT_SECRET);
-        const user = await User.findById(id, {password: 0, createdAt: 0, updatedAt: 0})
-        req.user = user
-    } catch (error) {
-        if (error.name === "TokenExpiredError" || error.name === "JsonWebTokenError") {
-            throw requestError(401, "Token is not valid");
+    const { id } = jwt.verify(token, JWT_SECRET, {}, (err, data) => {
+        if (err) {
+            throw requestError(401, err.message);
         }
-        throw error;
-    }
+
+        return data;
+    });
+    req.user = await User.findById(id, { password: 0, createdAt: 0, updatedAt: 0 });
 
     next();
+
+    // const authHeader = req.headers.authorization || "";
+    // const [type, token] = authHeader.split(" ");
+
+    // if (type !== "Bearer") {
+    //     throw requestError(401, "Token type is not allowed");
+    // }
+
+    // if (!token) {
+    //     throw requestError(401, "Token is required");
+    // }
+
+    // try {
+    //     const { id } = jwt.verify(token, JWT_SECRET);
+    //     const user = await User.findById(id, { password: 0, createdAt: 0, updatedAt: 0 });
+    //     req.user = user;
+    // } catch (error) {
+    //     if (error.name === "TokenExpiredError" || error.name === "JsonWebTokenError") {
+    //         throw requestError(401, "Token is not valid");
+    //     }
+    //     throw error;
+    // }
+
+    // next();
 };
 
-module.exports = authorization;
+module.exports = {authorization: wrapper(authorization)};
--- lesson_08/02_add_jwt_to_project/routes/api/user.js
+++ lesson_08/02_add_jwt_to_project/routes/api/user.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../../controllers/user')
-const auth = require('../../middlewares/authorizationMiddleware')
-const wrapper = require('../../helpers/controllerWrapper')
+const { authorization: auth }  = require('../../middlewares/authorizationMiddleware')
 
-// router.use(auth)
+router.use(auth)
 
-router.post('', wrapper(auth), controller.addBook)
-router.get('', wrapper(auth), controller.getBooks)
-router.get('/info', wrapper(auth), controller.getInfo)
+router.post('', controller.addBook)
+router.get('', controller.getBooks)
+router.get('/info', controller.getInfo)
 
 // router.post('', auth, controller.addBook)
 // router.get('', auth, controller.getBooks)
 // router.get('/info', auth, controller.getInfo)
+
+// router.post('', wrapper(auth), controller.addBook)
+// router.get('', wrapper(auth), controller.getBooks)
+// router.get('/info', wrapper(auth), controller.getInfo)
 
 module.exports = router;
